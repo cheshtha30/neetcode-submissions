ac8e1709e2413845ebd242977405a2e3a84fa1a3
cheshtha30 <98456792+cheshtha30@users.noreply.github.com>
2026-05-19 22:51:23 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        n = len(people)
+        left = 0
+        right = n-1
+        boats = 0
+
+        while left <= right :
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+                boats += 1
+
+            else:
+                right -=1
+                boats +=1
+
+        return boats 
+
+
+           
+
+        
