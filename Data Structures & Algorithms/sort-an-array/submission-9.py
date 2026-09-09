958054dd4934997c296d1a4711a505aedbcd7619
cheshtha30 <98456792+cheshtha30@users.noreply.github.com>
2026-09-09 17:23:53 +0530

    Add: sort-an-array - submission-9

---
+++ Data Structures & Algorithms/sort-an-array/submission-9.py
@@ -0,0 +1,112 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(n- i - 1):
+        #         if nums[j] > nums[j+1]:
+        #             nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        # return nums  
+        # n = len(nums)
+        # for i in range(n):
+        #     min_index = i
+        #     for j in range(i+1 , n):
+        #         if nums[j] < nums[min_index]:
+        #             min_index = j
+
+        #     nums[i], nums[min_index] = nums[min_index], nums[i]
+
+        # return nums 
+        # [10|,9,1,1,1,2,3,1] check 
+        #   j  i 
+        #   10> 9
+        # [10| 10 1 1 1 2 3 1]
+        #    j = -1
+        #    nums[0] = 9
+        # [9 10 | 1 1 1 2 3 1]
+        # n  = len(nums)
+        # for i in range(1 , n):
+        #     current = nums[i]
+        #     j = i-1
+        #     while j >=0 and nums[j]> current:
+        #         nums[j+1] = nums[j]
+        #         j-= 1
+
+        #     nums[j+1] = current
+
+    #     # return nums 
+    #     if len(nums) <= 1:
+    #         return nums
+
+    #     mid = len(nums)//2
+    #     left= nums[:mid]
+    #     right= nums[mid:]
+
+    #     left = self.sortArray(left)
+    #     right = self.sortArray(right)
+    #     return self.merge(left, right)
+
+    # def merge(self, left, right):
+    #     result = []
+    #     i = 0
+    #     j = 0
+    #     while i < len(left) and j < len(right):
+    #         if left[i] < right[j]:
+    #             result.append(left[i])
+    #             i += 1
+    #         else:
+    #             result.append(right[j])
+    #             j +=1
+
+    #     while i < len(left):
+    #         result.append(left[i])
+    #         i+= 1
+
+    #     while j < len(right):
+    #         result.append(right[j])
+    #         j+=1
+
+    #     return result 
+        # if len(nums) <= 1:
+        #     return nums
+
+        # left = []
+        # right = []
+        # equal = []
+
+        # pivot = nums[len(nums)//2]
+
+        # for num in nums:
+        #    if num < pivot:
+        #      left.append(num)
+        #    elif num > pivot:
+        #      right.append(num)
+        #    else:
+        #      equal.append(num)
+
+        # return self.sortArray(left) + equal + self.sortArray(right)
+        n = len(nums)
+        for i in range(n//2 -1, -1, -1):
+          self.heapify(nums, n, i)
+
+        #move biggest element to the end 
+        for i in range(n-1, 0 , -1):
+          nums[0], nums[i] = nums[i], nums[0]
+          self.heapify(nums, i , 0)
+        return nums
+
+    def heapify(self, nums, n, i):
+      largest = i
+      left = 2*i + 1
+      right = 2 * i + 2
+
+      if left < n and nums[left] > nums[largest]:
+        largest = left
+
+      if right <n and nums[right] > nums[largest]:
+        largest = right
+
+      if largest != i:
+        nums[i], nums[largest] = nums[largest], nums[i]
+        self.heapify(nums, n , largest)
